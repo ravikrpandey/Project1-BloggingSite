11b6a8f11004edfebaee3ac1aa8500203dacfcf4
ravikrpandey <104829126+ravikrpandey@users.noreply.github.com>
2022-06-21 13:06:31 +0530

    Dependencies updated

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://ravikrpandey:ravikrpandey@cluster0.nghbsfi.mongodb.net/ravikrpandeyDB", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
