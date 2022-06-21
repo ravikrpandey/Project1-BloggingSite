7ff862b2fa8aed79bd0c96f6b49c81e0647442f5
chetang12 <102719631+chetang12@users.noreply.github.com>
2022-06-21 16:09:29 +0530

    blogmodel chetan

---
+++ src/Model/BlogModel1.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+let ObjectId = mongoose.Schema.Types.ObjectId
+const blogSchema = new mongoose.Schema({
+    title: {
+        type : String,
+        required: true 
+    },
+    body:{
+        type : String,
+        required: true    
+    },
+    authorId:{
+        type: ObjectId,         
+        ref: 'Auther' ,
+        required: true
+    },
+    tags: [String], 
+    category:{
+        type: String,
+        required: true
+    },
+    subcategory: [String],
+    isDeleted: {
+        type: Boolean,default: false
+    },
+    publishedAt: {
+        type: Date, 
+    },
+    isPublished: {
+        type: Boolean, default: false
+    }
+
+}, { timestamps: true })
+
+
+module.exports = mongoose.model('Blog', blogSchema)
