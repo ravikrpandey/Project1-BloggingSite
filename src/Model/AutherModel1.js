d968638e3a3a97405f2ee286e4fc877a89bced4d
ravikrpandey <104829126+ravikrpandey@users.noreply.github.com>
2022-06-21 14:59:52 +0530

    checking

---
+++ src/Model/AutherModel1.js
@@ -0,0 +1,37 @@
+const Express = require('express')
+const { default: mongoose } = require('mongoose')
+const Mongoose = require('mongoose') 
+
+const autherSchema = new mongoose.Schema( {
+
+   fname: {
+    type: String,
+    required: true
+   },
+
+   lname: {
+    type: String,
+    required: true
+   },
+
+   title: {
+    type: String,
+    required: true,
+    enum: ["Mr", "Mrs", "Miss"]
+   },
+
+   email: {
+    type: String,
+    unique: true
+   },
+
+   password: {
+    type: String,
+    required: true
+   }
+
+
+
+},{timestamps: true});
+
+module.exports = mongoose.model('Auther', autherSchema);
